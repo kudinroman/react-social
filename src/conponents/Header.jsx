c0e7f75dafde2461de5542922591f1131912968c
Roman Kudin <kudinroman82@gmail.com>
2019-11-30 11:22:20 +0300

    first commit

---
+++ src/conponents/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSVw4T7bDXML_CjK98Lwi5MKtVXnveNrS4CvewKPZckIAyxFPOU" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/conponents/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <main className='content'>
+            <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTd52CzEsken4eT-A3O_obU1gvFuRGRkYBjoqpYBEIZ1jBMHqzX" /></div>
+            <div>
+                <span>ava + name</span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSAaTwfGm04U7igS8APiDslolRcWQk5Jr-MiHVVQYFDi0COduwl" /></div>
+            <div>post 1</div>
+            <div>post 2</div>
+            <div>post 3</div>
+        </main>
+    );
+}
+
+export default Profile;
